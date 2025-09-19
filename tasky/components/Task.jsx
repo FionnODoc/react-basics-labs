ae11d0263a7cfad5f40fd025c6fd931a24b02531
fionnodochartaigh <20106639@mail.wit.ie>
2025-09-19 14:24:54 +0100

    Added task state

---
+++ tasky/components/Task.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Task = (props) => {
+    return (
+        <div className="card">
+            <p>{props.title}</p>
+            <p>Due: {props.deadline}</p>
+            <div className='description'><p>{props.description}</p></div>
+            
+        </div>
+    )
+
+
+
+
+}
+
+
+
+
+export default Task;
+
