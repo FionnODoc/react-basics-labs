be70b62399a6c543fd0a1bd52189cba83e9954e9
fionnodochartaigh <20106639@mail.wit.ie>
2025-09-19 14:47:27 +0100

    Tasky Lab 2a - Exercise done

--- tasky/components/Task.jsx
+++ tasky/components/Task.jsx
@@ -6,6 +6,7 @@ const Task = (props) => {
             <p>{props.title}</p>
             <p>Due: {props.deadline}</p>
             <div className='description'><p>{props.description}</p></div>
+            <div className='priority'><p>{props.priority}</p></div>
             
         </div>
     )
--- tasky/src/App.jsx
+++ tasky/src/App.jsx
@@ -3,13 +3,14 @@ import Task from '../components/Task';
 import React, {useState} from 'react';
 
 function App() {
-    const [ taskState, setTaskState ] = useState({
+  const [ taskState, setTaskState ] = useState({
     tasks: [
-      { title:"Dishes", description: "Empty dishwasher", deadline: "Today" },
-      { title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow" },
-      { title: "Tidy up", deadline: "Today" }
+      { id: 1, title:"Dishes", description: "Empty dishwasher", deadline: "Today",priority:"Low" },
+      { id: 2, title: "Laundry", description: "Fold clothes and put them away", deadline: "Tomorrow", priority:"Medium" },
+      { id: 3, title: "Tidy up", deadline: "Today", priority:"High" },
     ]
   });
+
 
   return (
     <div className="container">
@@ -19,6 +20,9 @@ function App() {
       title={task.title}
       description={task.description}
       deadline={task.deadline}
+      priority={task.priority}
+      key={task.id}
+      
     />
   ))} 
 
