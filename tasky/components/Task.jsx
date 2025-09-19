3958699f167a2de03fa48ecb9dcd13bf05251923
fionnodochartaigh <20106639@mail.wit.ie>
2025-09-19 15:08:50 +0100

    Mark task as done

--- tasky/components/Task.jsx
+++ tasky/components/Task.jsx
@@ -7,6 +7,9 @@ const Task = (props) => {
             <p>Due: {props.deadline}</p>
             <div className='description'><p>{props.description}</p></div>
             <div className='priority'><p>{props.priority}</p></div>
+            <button onClick={props.markDone}>Done</button>
+
+
             
         </div>
     )
